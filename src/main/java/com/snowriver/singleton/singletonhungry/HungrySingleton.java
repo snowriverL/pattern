94a4feadbb7c55fa42cce485d15eb1f2ca9200bc
m18210540611@163.com <gfh261022>
2019-03-11 16:04:57 +0800

    饿汉式单例

--- src/main/java/com/snowriver/singleton/singletonhungry/HungrySingleton.java
+++ src/main/java/com/snowriver/singleton/singletonhungry/HungrySingleton.java
@@ -1,4 +1,25 @@
 package com.snowriver.singleton.singletonhungry;
 
+/**
+ * 绝对线程安全，在线程还没出现以前就是实例化了，不可能存在访问安全问题
+ */
 public class HungrySingleton {
+
+    private static final HungrySingleton hungrySingleton = new HungrySingleton();
+
+    /**
+     *     静态代码块
+     *     private static final HungrySingleton hungrySingleton;
+     *     static {
+     *         hungrySingleton = new HungrySingleton();
+     *     }
+     */
+
+
+    private HungrySingleton () {}
+
+    public HungrySingleton getInstance() {
+        return hungrySingleton;
+    }
+
 }
